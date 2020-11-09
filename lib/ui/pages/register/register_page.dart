6d0b66668ffe0b2abac74db1789052297a18ef8b
fabiovivas <fvivas1982@gmail.com>
2020-11-09 19:18:57 -0300

    add RegisterPage layout

--- lib/ui/pages/register/register_page.dart
+++ lib/ui/pages/register/register_page.dart
@@ -24,7 +24,96 @@ class RegisterPage extends StatelessWidget {
               ),
             ),
           )),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 37, 10, 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 17,
+                  fontWeight: FontWeight.w400,
+                ),
+                children: <TextSpan>[
+                  TextSpan(text: 'Para começar '),
+                ],
+              ),
+            ),
+            RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 17,
+                  fontWeight: FontWeight.w400,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: 'qual o seu ',
+                  ),
+                  TextSpan(
+                    text: 'e-mail?',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            TextField(decoration: InputDecoration(hintText: 'Digite aqui'))
+          ],
+        ),
+      ),
+      bottomNavigationBar: Transform.translate(
+        offset: Offset(0.0, -1 * MediaQuery.of(context).viewInsets.bottom),
+        child: BottomAppBar(
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: 65,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'CANCELAR',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      color: Colors.white,
+                      elevation: 1,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: 65,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'PRÓXIMO',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      color: Color.fromRGBO(249, 202, 7, 1),
+                      elevation: 1,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
